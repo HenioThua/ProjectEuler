8f535165810b3da3765f396dd01181f66275e71c
Hênio Thuãn <heniothua13@gmail.com>
2022-09-14 19:13:14 -0300

    esse cara deu trabalho

--- euler22.py
+++ euler22.py
@@ -1,3 +1,4 @@
+letters = set()
 nomes = open("names.txt", "r")
 nomes = nomes.read()
 def transform_lista(a):
@@ -6,15 +7,27 @@ def transform_lista(a):
 def remove_aspas(b):
     return b.replace('"', "")
 
+def remove_new_line(t):
+    return t.replace("\n", "")
 
-        
-ordem_alfa = sorted(transform_lista(remove_aspas(nomes)))
-cont = 0
+def calc_alfa_value(r):
+    cont = 0
+    for i in r:
+        if i == "\n":
+            print(r, "tem \\n")
+        letters.add(i)
+        cont += ord(i) - 64
+    return cont
+     
+ordem_alfa = sorted(transform_lista(remove_new_line(remove_aspas(nomes))))
+cont = 0 
 valor_total = 0
 for i in range(len(ordem_alfa)):
-    for letter in ordem_alfa[i]:
-        cont += ord(letter)-64
-    valor_total += cont * i
-print(i)
-        
+    valor_total += calc_alfa_value(ordem_alfa[i]) * (i+1)
+print(valor_total)
+
+assert calc_alfa_value("COLIN") == 53
+assert ordem_alfa[937] == "COLIN"
+
+
 
